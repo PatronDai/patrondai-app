ed45aa2e55db0aec24c661006181fb2eafec44fa
Piotr Adamczyk <xeno@xeno.yt>
2020-02-29 15:39:56 +0100

    torus without button

--- src/App.js
+++ src/App.js
@@ -8,31 +8,54 @@ import Torus from "@toruslabs/torus-embed";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      addresses: [],
+      initialized: false,
+      loginInProgress: false
+    };
   }
   async componentDidMount() {
     this.torus = new Torus();
-    await this.torus.init();
-    await this.torus.login();
-    this.ethereum = new ethers.providers.Web3Provider(this.torus.provider);
+    await this.torus.init({
+      showTorusButton: false,
+      enableLogging: true,
+      network: { host: "rinkeby" }
+    });
+    this.setState({ initialized: true });
   }
   render() {
+    const { addresses, initialized, loginInProgress } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {initialized ? (
+          <div>
+            {loginInProgress ? (
+              <div>Logging in...</div>
+            ) : (
+              <div>
+                {!addresses.length ? (
+                  <button
+                    onClick={async () => {
+                      try {
+                        this.setState({ loginInProgress: true });
+                        this.setState({ addresses: await this.torus.login() });
+                        this.setState({ loginInProgress: false });
+                      } catch (error) {
+                        console.error(error);
+                      }
+                    }}
+                  >
+                    Login
+                  </button>
+                ) : (
+                  addresses
+                )}
+              </div>
+            )}
+          </div>
+        ) : (
+          <div>Wait for it...</div>
+        )}
       </div>
     );
   }
